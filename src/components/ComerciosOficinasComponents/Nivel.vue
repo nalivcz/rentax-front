b48dd4eb07ead3ba0e8d00d2dff94701079169ba
Nadia Livier <nalivcz@gmail.com>
2021-03-21 02:54:28 -0700

    se termino desarrollo de escritorio-web

---
+++ src/components/ComerciosOficinasComponents/Nivel.vue
@@ -0,0 +1,87 @@
+<template>
+    <div style="background-color:#f2f2f2; padding:5%;" class="text-center">
+		<span style="font-size:36px; color:#56555a;" class="CircularStd-Black">NIVEL 1</span>
+		<div style="display:flex; flex-direction : row;justify-content: space-between; ">
+			<div style="width:50%; display:flex; flex-direction:column; align-items:center;" >
+				<div>
+				<img src="../../assets/comercialoficinas/centrobalcones/imagenes/Planos/planonivel1.jpg" style="margin-left:10%;" width="85%">
+				<div style="font-size:24px; color:#d70a48; width:55%; text-align:left; margin-left:20%; padding:10px; line-height:30px;" class="CircularStd-Book">Precio de renta por m<sup>2</sup> en NIVEL 1: $230</div>
+				</div>
+			</div>
+			<div style="width:50%; padding-left:2%; padding-right:5%; display:flex; flex-direction:column; align-items: flex-start;">
+				<div style="display:flex; flex-direction:column;">
+					<table border="3" class="CircularStd-Bold" >
+						<thead style="background-color :#d70a48; color:white;">
+						<th style="padding-left: 25px; padding-right: 25px;">NIVEL 1</th>
+						<th style="padding-left: 50px; padding-right: 50px;">ÁREA</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>LOCAL 1</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 2</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 3</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 4</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 5</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 6</td>
+								<td>52.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 7</td>
+								<td>53.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 8</td>
+								<td>55.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 9</td>
+								<td>57.00 m<sup>2</sup></td>
+							</tr>
+						</tbody>
+						<tfoot style="background-color :#414046; color:white;">
+						<th>TOTAL</th>
+						<th>466.00 m<sup>2</sup></th>
+						</tfoot>
+					</table>
+					<div style="display:flex; flex-direction:row; margin-top:20px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #d70a48;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá de renta</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #c7c7c7;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areas Comunes</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #ffaa61;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Cajones de Estacionamiento</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #38b04d;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá Verde</div>
+					</div>
+					<div style="font-size:24px; text-align:left; color:#57565b; width:70%; line-height:30px;" class="CircularStd-Book">Servicio de mantenimiento de $25 por m<sup>2</sup></div>
+				</div>
+			</div>
+		</div>
+		<hr style=" margin-left:5%; margin-right:5%; margin-top:5%; border-top: 5px dashed gray;">
+	</div>
+</template>
--- src/views/desarrollos/CentroBalcones.vue
+++ src/views/desarrollos/CentroBalcones.vue
@@ -38,19 +38,355 @@
 			<img src="../../assets/comercialoficinas/centrobalcones/imagenes/Planos/Croquis.jpg" width="100%">
 		</div>
 	</div>
-	<div style="background-color:#f2f2f2;" class="text-center">
-		Nivel 1
-		<div style="display:flex; flex-direction : row; align-items:center;justify-content: space-between; ">
-		<div style="width:50%; padding-left:10%; padding-right:5%">
-			<div style="padding-left:7%; font-size:18px; color:#56555a; letter-spacing:1px; line-height: 1.2;" class="CircularStd-Book">
-				<span>Ubicado en una zona con importante desarrollo urbano en la ciudad de Culiacán, Sinaloa, Centro Balcones cuenta con espacios diseñados para cualquier tipo de negocio.</span>
-				<hr style="background-color:#4d4d4f; width:40%; border: 2px solid #d70a48; margin-top:60px;">
+	<div style="background-color:#f2f2f2; padding:5%;" class="text-center">
+		<span style="font-size:36px; color:#56555a;" class="CircularStd-Black">NIVEL 1</span>
+		<div style="display:flex; flex-direction : row;justify-content: space-between; ">
+			<div style="width:50%; display:flex; flex-direction:column; align-items:center;" >
+				<div>
+				<img src="../../assets/comercialoficinas/centrobalcones/imagenes/Planos/planonivel1.jpg" style="margin-left:10%;" width="85%">
+				<div style="font-size:24px; color:#d70a48; width:55%; text-align:left; margin-left:20%; padding:10px; line-height:30px;" class="CircularStd-Book">Precio de renta por m<sup>2</sup> en NIVEL 1: $230</div>
+				</div>
+			</div>
+			<div style="width:50%; padding-left:2%; padding-right:5%; display:flex; flex-direction:column; align-items: flex-start;">
+				<div style="display:flex; flex-direction:column;">
+					<table border="3" class="CircularStd-Bold" >
+						<thead style="background-color :#d70a48; color:white;">
+						<th style="padding-left: 25px; padding-right: 25px;">NIVEL 1</th>
+						<th style="padding-left: 50px; padding-right: 50px;">ÁREA</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>LOCAL 1</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 2</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 3</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 4</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 5</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 6</td>
+								<td>52.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 7</td>
+								<td>53.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 8</td>
+								<td>55.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 9</td>
+								<td>57.00 m<sup>2</sup></td>
+							</tr>
+						</tbody>
+						<tfoot style="background-color :#414046; color:white;">
+						<th>TOTAL</th>
+						<th>466.00 m<sup>2</sup></th>
+						</tfoot>
+					</table>
+					<div style="display:flex; flex-direction:row; margin-top:20px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #d70a48;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá de renta</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #c7c7c7;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areas Comunes</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #ffaa61;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Cajones de Estacionamiento</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #38b04d;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá Verde</div>
+					</div>
+					<div style="font-size:24px; text-align:left; color:#57565b; width:70%; line-height:30px;" class="CircularStd-Book">Servicio de mantenimiento de $25 por m<sup>2</sup></div>
+				</div>
 			</div>
 		</div>
-		<div style="width:50%">
-			<img src="../../assets/comercialoficinas/centrobalcones/imagenes/Planos/Croquis.jpg" width="100%">
-		</div>
+		<hr style=" margin-left:5%; margin-right:5%; border-top: 5px dashed gray;">
 	</div>
+	<div style="background-color:#f2f2f2; padding:5%;" class="text-center">
+		<span style="font-size:36px; color:#56555a;" class="CircularStd-Black">NIVEL 1</span>
+		<div style="display:flex; flex-direction : row;justify-content: space-between; ">
+			<div style="width:50%; display:flex; flex-direction:column; align-items:center;" >
+				<div>
+				<img src="../../assets/comercialoficinas/centrobalcones/imagenes/Planos/planonivel1.jpg" style="margin-left:10%;" width="85%">
+				<div style="font-size:24px; color:#d70a48; width:55%; text-align:left; margin-left:20%; padding:10px; line-height:30px;" class="CircularStd-Book">Precio de renta por m<sup>2</sup> en NIVEL 1: $230</div>
+				</div>
+			</div>
+			<div style="width:50%; padding-left:2%; padding-right:5%; display:flex; flex-direction:column; align-items: flex-start;">
+				<div style="display:flex; flex-direction:column;">
+					<table border="3" class="CircularStd-Bold" >
+						<thead style="background-color :#d70a48; color:white;">
+						<th style="padding-left: 25px; padding-right: 25px;">NIVEL 1</th>
+						<th style="padding-left: 50px; padding-right: 50px;">ÁREA</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>LOCAL 1</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 2</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 3</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 4</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 5</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 6</td>
+								<td>52.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 7</td>
+								<td>53.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 8</td>
+								<td>55.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 9</td>
+								<td>57.00 m<sup>2</sup></td>
+							</tr>
+						</tbody>
+						<tfoot style="background-color :#414046; color:white;">
+						<th>TOTAL</th>
+						<th>466.00 m<sup>2</sup></th>
+						</tfoot>
+					</table>
+					<div style="display:flex; flex-direction:row; margin-top:20px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #d70a48;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá de renta</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #c7c7c7;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areas Comunes</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #ffaa61;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Cajones de Estacionamiento</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #38b04d;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá Verde</div>
+					</div>
+					<div style="font-size:24px; text-align:left; color:#57565b; width:70%; line-height:30px;" class="CircularStd-Book">Servicio de mantenimiento de $25 por m<sup>2</sup></div>
+				</div>
+			</div>
+		</div>
+		<hr style=" margin-left:5%; margin-right:5%; margin-top:5%; border-top: 5px dashed gray;">
+	</div>
+	<div style="background-color:#f2f2f2; padding:5%;" class="text-center">
+		<span style="font-size:36px; color:#56555a;" class="CircularStd-Black">NIVEL 2</span>
+		<div style="display:flex; flex-direction : row;justify-content: space-between; ">
+			<div style="width:50%; display:flex; flex-direction:column; align-items:center;" >
+				<div>
+				<img src="../../assets/comercialoficinas/centrobalcones/imagenes/Planos/planonivel2.jpg" style="margin-left:10%;" width="85%">
+				<div style="font-size:24px; color:#d70a48; width:55%; text-align:left; margin-left:20%; padding:10px; line-height:30px;" class="CircularStd-Book">Precio de renta por m<sup>2</sup> en NIVEL 1: $230</div>
+				</div>
+			</div>
+			<div style="width:50%; padding-left:2%; padding-right:5%; display:flex; flex-direction:column; align-items: flex-start;">
+				<div style="display:flex; flex-direction:column;">
+					<table border="3" class="CircularStd-Bold" >
+						<thead style="background-color :#d70a48; color:white;">
+						<th style="padding-left: 25px; padding-right: 25px;">NIVEL 2</th>
+						<th style="padding-left: 50px; padding-right: 50px;">ÁREA</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>LOCAL 1</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 2</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 3</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 4</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 5</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 6</td>
+								<td>52.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 7</td>
+								<td>53.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 8</td>
+								<td>55.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 9</td>
+								<td>57.00 m<sup>2</sup></td>
+							</tr>
+						</tbody>
+						<tfoot style="background-color :#414046; color:white;">
+						<th>TOTAL</th>
+						<th>466.00 m<sup>2</sup></th>
+						</tfoot>
+					</table>
+					<div style="display:flex; flex-direction:row; margin-top:20px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #d70a48;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá de renta</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #c7c7c7;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areas Comunes</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #ffaa61;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Cajones de Estacionamiento</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #38b04d;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá Verde</div>
+					</div>
+					<div style="font-size:24px; text-align:left; color:#57565b; width:70%; line-height:30px;" class="CircularStd-Book">Servicio de mantenimiento de $25 por m<sup>2</sup></div>
+				</div>
+			</div>
+		</div>
+		<hr style=" margin-left:5%; margin-right:5%; margin-top:5%; border-top: 5px dashed gray;">
+	</div>
+	<div style="background-color:#f2f2f2; padding:5%;" class="text-center">
+		<span style="font-size:36px; color:#56555a;" class="CircularStd-Black">NIVEL 3</span>
+		<div style="display:flex; flex-direction : row;justify-content: space-between; ">
+			<div style="width:50%; display:flex; flex-direction:column; align-items:center;" >
+				<div>
+				<img src="../../assets/comercialoficinas/centrobalcones/imagenes/Planos/planonivel3.jpg" style="margin-left:10%;" width="85%">
+				<div style="font-size:24px; color:#d70a48; width:55%; text-align:left; margin-left:20%; padding:10px; line-height:30px;" class="CircularStd-Book">Precio de renta por m<sup>2</sup> en NIVEL 1: $230</div>
+				</div>
+			</div>
+			<div style="width:50%; padding-left:2%; padding-right:5%; display:flex; flex-direction:column; align-items: flex-start;">
+				<div style="display:flex; flex-direction:column;">
+					<table border="3" class="CircularStd-Bold" >
+						<thead style="background-color :#d70a48; color:white;">
+						<th style="padding-left: 25px; padding-right: 25px;">NIVEL 3</th>
+						<th style="padding-left: 50px; padding-right: 50px;">ÁREA</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>LOCAL 1</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 2</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 3</td>
+								<td>46.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 4</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 5</td>
+								<td>40.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 6</td>
+								<td>52.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 7</td>
+								<td>53.50 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 8</td>
+								<td>55.00 m<sup>2</sup></td>
+							</tr>
+							<tr>
+								<td>LOCAL 9</td>
+								<td>57.00 m<sup>2</sup></td>
+							</tr>
+						</tbody>
+						<tfoot style="background-color :#414046; color:white;">
+						<th>TOTAL</th>
+						<th>466.00 m<sup>2</sup></th>
+						</tfoot>
+					</table>
+					<div style="display:flex; flex-direction:row; margin-top:20px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #d70a48;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá de renta</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #c7c7c7;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areas Comunes</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #ffaa61;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Cajones de Estacionamiento</div>
+					</div>
+					<div style="display:flex; flex-direction:row; margin-top:10px; align-items:baseline;">
+						<div style="height: 20px;width: 30px;background-color: #38b04d;">
+						</div>
+						<div class="CircularStd-Book" style="font-size:24px; margin-left:10px; color:#56555a;">Areá Verde</div>
+					</div>
+					<div style="font-size:24px; text-align:left; color:#57565b; width:70%; line-height:30px;" class="CircularStd-Book">Servicio de mantenimiento de $25 por m<sup>2</sup></div>
+				</div>
+			</div>
+		</div>
+		<hr style=" margin-left:5%; margin-right:5%; margin-top:5%; border-top: 5px dashed gray;">
+	</div>
+	<div style="background-color:#f2f2f2; padding:5%;" class="text-center">
+		<span style="font-size:36px; color:#56555a;" class="CircularStd-Black">AZOTEA</span>
+		<div style="display:flex; flex-direction : row;justify-content: space-between; ">
+			<div style="width:100%; display:flex; flex-direction:column; align-items:center;" >
+				<div>
+				<img src="../../assets/comercialoficinas/centrobalcones/imagenes/Planos/azotea.jpg" style="margin-left:10%;" width="70%">
+				</div>
+			</div>	
+		</div>
 	</div>
 	<div style="background-color : #d80a48" class="CircularStd-Book" >
 		<center><h1 style="color:white; letter-spacing:6px;">CONTACTO</h1></center>
@@ -71,12 +407,23 @@ export default {
 </script>
 
 <style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th {
+  text-align: center;    
+}
+tr:nth-child(even) {
+  background-color: #d70a48;
+}
+tr:nth-child(odd) {
+   background-color: #c4c4c4;
+}
 ul {
   list-style: none; /* Remove HTML bullets */
   
 }
-
-
 li::before {
   content: "•"; /* Insert content that looks like bullets */
   padding-right: 4px;
