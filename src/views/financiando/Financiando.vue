7c3e1515e66243f6c7ad0d8885e1ad27d30f1f1b
Nadia Livier <nalivcz@gmail.com>
2021-03-30 02:11:15 -0700

    se termino parte movil de vista financiando, falta añadir un icono

--- src/views/financiando/Financiando.vue
+++ src/views/financiando/Financiando.vue
@@ -1,43 +1,91 @@
 <template>
   <v-app>
-      <div :style="{ backgroundImage: `url(${require('@/assets/financiando/financiando.jpg')})` , backgroundSize : '100% 100%', height : '600px'  , display : 'flex', flexDirection : 'column' }">
-            <div style="display: flex; flex-direction: column; justify-content: center; height:100%;">
-                <div style="display: flex; flex-direction: row; justify-content: center;">
-                    <div style="display:flex; flex-direction : column">
-                        <span  style=" font-size:36px; color: #4d4d4f; letter-spacing:16px">FINANCIANDO </span>
-                        <span style=" font-size:36px; color: #4d4d4f; letter-spacing:16px" class="CircularStd-Bold">TU PROYECTO</span> 
+      <div class="hidden-sm-and-down">
+        <div :style="{ backgroundImage: `url(${require('@/assets/financiando/financiando.jpg')})` , backgroundSize : '100% 100%', height : '600px'  , display : 'flex', flexDirection : 'column' }">
+                <div style="display: flex; flex-direction: column; justify-content: center; height:100%;">
+                    <div style="display: flex; flex-direction: row; justify-content: center;">
+                        <div style="display:flex; flex-direction : column">
+                            <span  style=" font-size:36px; color: #4d4d4f; letter-spacing:16px">FINANCIANDO </span>
+                            <span style=" font-size:36px; color: #4d4d4f; letter-spacing:16px" class="CircularStd-Bold">TU PROYECTO</span> 
+                        </div>
                     </div>
                 </div>
-            </div>
+        </div>
+        <div style="display: flex; flex-direction: column; justify-content: center; padding-left :20%; padding-right :20%; padding-top: 2%;">
+                <div style="">De la mano de <span style="color:green" class="CircularStd-Bold">Rentax</span>, las familias que han buscado financiamiento para construir su hogar han sido casos de éxito y se les ha ayudado a resolver cualquier detalle con el <span class="CircularStd-BookItalic">Buro de Crédito</span>. Con el Crédito de Construcción que se utiliza para <span class="CircularStd-Bold">Your Home</span>(construir tu casa a la medida), el banco te puede prestar hasta el 50% para comprar el terreno o hasta el 70% para la construcción, tasas de interes del 7.75% y plazos de hasta 20 años.   </div>
+                <div style="display: flex; flex-direction: row; justify-content: center;">
+                    <img :src="require(`@/assets/financiando/logos/socasesoreshipotecarios.png`)"
+                    width="250px"
+                    height="100px">  
+                </div>
+                <div class="CircularStd-Bold" style="display: flex; flex-direction: row; justify-content: center; margin-top:20px; background-color: #3ab14d; color:white; font-size : 22px; letter-spacing:2px; padding-top:2%;  padding-bottom:2%;">
+                    CONOCE NUESTROS PROYECTOS ENTREGADOS
+                </div>
+                <div style="margin-top:10px;">
+                    <v-row>
+                        <v-col
+                        v-for="item in items"
+                        :key="item"
+                        class="d-flex child-flex"
+                        cols="2"
+                        >
+                        <img
+                            :src="require(`@/assets/financiando/logos/${item.srcImage}`)"
+                            :lazy-src="require(`@/assets/financiando/logos/${item.srcImage}`)"
+                            aspect-ratio="1"
+                            width="100%"
+                            class="grey lighten-2"
+                        >
+                        
+                        </v-col>
+                    </v-row>
+                </div>
+        </div>
       </div>
-      <div style="display: flex; flex-direction: column; justify-content: center; padding-left :20%; padding-right :20%; padding-top: 2%;">
-            <div style="">De la mano de <span style="color:green" class="CircularStd-Bold">Rentax</span>, las familias que han buscado financiamiento para construir su hogar han sido casos de éxito y se les ha ayudado a resolver cualquier detalle con el <span class="CircularStd-BookItalic">Buro de Crédito</span>. Con el Crédito de Construcción que se utiliza para <span class="CircularStd-Bold">Your Home</span>(construir tu casa a la medida), el banco te puede prestar hasta el 50% para comprar el terreno o hasta el 70% para la construcción, tasas de interes del 7.75% y plazos de hasta 20 años.   </div>
-            <div style="display: flex; flex-direction: row; justify-content: center;">
-                <img :src="require(`@/assets/financiando/logos/socasesoreshipotecarios.png`)"
-                width="250px"
-                height="100px">  
-            </div>
-            <div class="CircularStd-Bold" style="display: flex; flex-direction: row; justify-content: center; margin-top:20px; background-color: #3ab14d; color:white; font-size : 22px; letter-spacing:2px; padding-top:2%;  padding-bottom:2%;">
-                CONOCE NUESTROS PROYECTOS ENTREGADOS
-            </div>
-            <div style="margin-top:10px;">
-                <v-row>
-                    <v-col
-                    v-for="item in items"
-                    :key="item"
-                    class="d-flex child-flex"
-                    cols="2"
-                    >
-                    <img
-                        :src="require(`@/assets/financiando/logos/${item.srcImage}`)"
-                        :lazy-src="require(`@/assets/financiando/logos/${item.srcImage}`)"
-                        aspect-ratio="1"
-                        width="100%"
-                        class="grey lighten-2"
-                    >
-                    
-                    </v-col>
-                </v-row>
+      <div class="hidden-md-and-up">
+          <div :style="{ backgroundImage: `url(${require('@/assets/financiando/financiando.jpg')})` , backgroundSize : '100% 45%', height : '100%'  , display : 'flex', flexDirection : 'column' }">
+                <div style="display: flex; flex-direction: column; justify-content: center; height:100%px;">
+                    <div style="display: flex; flex-direction: row; justify-content: center; margin-top:20%;">
+                        <div style="display:flex; flex-direction : column">
+                            <center>
+                            <hr style="background-color:#3ab14d; width:40%; border: 2px solid #3ab14d; margin-top:60px; margin-bottom:10%;">
+                            </center>
+                            <span  style=" font-size:24px; color: #4d4d4f;" class="CircularStd-Bold">FINANCIANDO </span>
+                            <span style=" font-size:24px; color: #4d4d4f; letter-spacing:1px" class="CircularStd-Bold">TU PROYECTO</span> 
+                        </div>
+                    </div> 
+                    <div style="display: flex; flex-direction: column; justify-content: center; padding-left :10%; padding-right :10%; padding-top: 2%; margin-top:5%;">
+                        <div style="">De la mano de <span style="color:green" class="CircularStd-Bold">Rentax</span>, las familias que han buscado financiamiento para construir su hogar han sido casos de éxito y se les ha ayudado a resolver cualquier detalle con el <span class="CircularStd-BookItalic">Buro de Crédito</span>. Con el Crédito de Construcción que se utiliza para <span class="CircularStd-Bold">Your Home</span>(construir tu casa a la medida), el banco te puede prestar hasta el 50% para comprar el terreno o hasta el 70% para la construcción, tasas de interes del 7.75% y plazos de hasta 20 años.   </div>
+                        <div style="display: flex; flex-direction: row; justify-content: center; margin-top:5%;">
+                            <img :src="require(`@/assets/financiando/logos/socasesoreshipotecarios.png`)"
+                            width="50%px"
+                            height="80%px">  
+                        </div>
+                        <div class="CircularStd-Bold" style="display: flex; flex-direction: row; justify-content: center; margin-top:20px; background-color: #3ab14d; color:white; font-size : 10px; letter-spacing:1px; padding-top:2%;  padding-bottom:2%;">
+                            CONOCE NUESTROS PROYECTOS ENTREGADOS
+                        </div>
+                        <div style="margin-top:10px;">
+                            <v-row>
+                                <v-col
+                                v-for="item in items"
+                                :key="item"
+                                class="d-flex child-flex"
+                                cols="2"
+                                >
+                                <img
+                                    :src="require(`@/assets/financiando/logos/${item.srcImage}`)"
+                                    :lazy-src="require(`@/assets/financiando/logos/${item.srcImage}`)"
+                                    aspect-ratio="1"
+                                    width="100%"
+                                    class="grey lighten-2"
+                                >
+                                
+                                </v-col>
+                            </v-row>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
       </div>
   </v-app>
